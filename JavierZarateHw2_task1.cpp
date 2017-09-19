a015dd1dce8c5c90cabf73d30cfb0e697d4b0003
zarate8 <chuy8jay@gmail.com>
2017-09-19 13:00:59 -0600

    First commit

---
+++ JavierZarateHw2_task1.cpp
@@ -0,0 +1,25 @@
+
+// File: JavierZarateHw2_task1
+// Created by Javier Zarate on 9/19/17 for CS1410.
+// Copyright (c) 2017 WSU
+//
+
+#include <iostream>
+using namespace std;
+
+// Constants
+
+
+// Prototypes
+
+
+// Main Program Program
+
+int main(void)
+{
+
+    return 0;
+
+}
+
+// Function Definitions
