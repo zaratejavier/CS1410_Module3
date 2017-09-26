360ffadb341529b10704faf63e47a4fe925600ed
zaratejavier <javierzaratemata@mail.weber.edu>
2017-09-26 12:26:38 -0600

    returning objects from functions

--- DistanceClassArguments.cpp
+++ DistanceClassArguments.cpp
@@ -41,6 +41,7 @@ public:
         cout<<"Feet "<< feet<<" and inches "<<inches<<endl;
     }
     void add_dist(Distance d1, Distance d2);
+    Distance add_dist_tome(Distance d1);
 };
 
 // Prototypes
@@ -70,6 +71,10 @@ int main(void)
     cout<<"d4 = ";
     d4.showDist();
 
+    d4= d4.add_dist_tome(d2);
+    cout<<"\nd4 new =";
+    d4.showDist();
+
     return 0;
 
 }
@@ -79,7 +84,7 @@ int main(void)
 // Template: CLASSNAME::Function_name()
 // 1)declare protptype inside class
 // 2)the scope resolution operator::
-//3) use the scope resolution
+//3) use the scope resolution operator::
 void Distance::add_dist(Distance d1, Distance d2)
 {
     inches = d1.inches+d2.inches; //add the inches
@@ -90,4 +95,17 @@ void Distance::add_dist(Distance d1, Distance d2)
         feet++;             //check for extra foot
     }
     feet += d1.feet + d2.feet;      //add feet
+}
+Distance Distance::add_dist_tome(Distance d1)
+{
+    Distance temp;
+    temp.inches = inches+ d1.inches;
+    if(temp.inches >= 12.0)
+    {
+    temp.inches -= 12.00;
+
+    temp.feet += 1;
+    }
+
+
 }
